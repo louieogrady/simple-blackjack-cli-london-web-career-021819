42de3fcb2520dc9f3459c7fedb7da5602c2235d5
Louie O'Grady <l.ogrady@gmail.com>
2019-01-25 15:33:09 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -12,8 +12,7 @@ def display_card_total(card_total)
 end
 
 def prompt_user
-  puts "Type 'h' to hit 's' to stay"
-  get_user_input
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
